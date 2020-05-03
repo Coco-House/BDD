b6fbcea5007045babff168f8e9de507afee2b622
kramossss <andria.manoa@yahoo.fr>
2020-05-03 21:42:55 +0200

    Màj MySQL

--- Scripts/Script MySQL/2_Creation_Tables.sql
+++ Scripts/Script MySQL/2_Creation_Tables.sql
@@ -2,30 +2,30 @@ USE `projet`;
 
 CREATE TABLE `projet`.`fournisseur`
 (
-	`codeF` VARCHAR(10) NOT NULL,
+	`idF` VARCHAR(10) NOT NULL,
 	`nomF` VARCHAR(20) NOT NULL,
-	`telF` INT NULL,
-	PRIMARY KEY (`codeF`)
+	`telF` INT NOT NULL,
+	PRIMARY KEY (`idF`)
 );
 
 CREATE TABLE `projet`.`produit`
 (
 	`nomP` VARCHAR(20) NOT NULL,
     `categorieP` VARCHAR(20) NOT NULL,
-	`quantite` INT NULL,
-    `stockActuel` INT NULL,
-    `stockMin` INT NULL,
-    `stockMax` INT NULL,
-    `codeF` VARCHAR(10) NOT NULL,
+	`quantite` INT NOT NULL,
+    `stockActuel` INT NOT NULL,
+    `stockMin` INT NOT NULL,
+    `stockMax` INT NOT NULL,
+    `idF` VARCHAR(10) NOT NULL,
 	PRIMARY KEY (`nomP`),
-    FOREIGN KEY (`codeF`) REFERENCES `projet`.`fournisseur`(`codeF`)
+    FOREIGN KEY (`idF`) REFERENCES `projet`.`fournisseur`(`idF`)
 );
 
 CREATE TABLE `projet`.`client`
 (
 	`idC` INT NOT NULL,
 	`nomC` VARCHAR(20) NOT NULL,
-	`telC` INT NULL,
+	`telC` INT NOT NULL,
 	PRIMARY KEY (`idC`)
 );
 
@@ -44,8 +44,8 @@ CREATE TABLE `projet`.`recette`
 	`nomR` VARCHAR(20) NOT NULL,
     `listeIngredients` VARCHAR(100) NOT NULL,
     `descriptionR` VARCHAR(100) NOT NULL,
-	`prixR` INT NULL,
-    `remunerationCuisinier` INT NULL,
+	`prixR` FLOAT NOT NULL,
+    `remunerationCuisinier` FLOAT NOT NULL,
     `idCdR` INT NOT NULL,
 	PRIMARY KEY (`idR`),
     FOREIGN KEY (`idCdR`) REFERENCES `projet`.`cdr`(`idCdR`)
--- Scripts/Script MySQL/3_Peuplement_Tables.sql
+++ Scripts/Script MySQL/3_Peuplement_Tables.sql
@@ -1,10 +1,34 @@
 -- insertion dans la table fournisseur
 
-INSERT INTO `projet`.`fournisseur` (`codeF`, `nomF`, `telF`) VALUES ('FX482', 'Distram', '0124359927');
-INSERT INTO `projet`.`fournisseur` (`codeF`, `nomF`, `telF`) VALUES ('FU155', 'Davigel', '0106149938');
-INSERT INTO `projet`.`fournisseur` (`codeF`, `nomF`, `telF`) VALUES ('FX751', 'Agidra', '0136928534');
-INSERT INTO `projet`.`fournisseur` (`codeF`, `nomF`, `telF`) VALUES ('FP330', 'MiamLand', '0126339604');
+INSERT INTO `projet`.`fournisseur` (`idF`,`nomF`,`telF`) VALUES ('FX482', 'Distram', 0124359927);
+INSERT INTO `projet`.`fournisseur` (`idF`,`nomF`,`telF`) VALUES ('FU155', 'Davigel', 0106149938);
+INSERT INTO `projet`.`fournisseur` (`idF`,`nomF`,`telF`) VALUES ('FX751', 'Agidra', 0136928534);
+INSERT INTO `projet`.`fournisseur` (`idF`,`nomF`,`telF`) VALUES ('FP330', 'MiamLand', 0126339604);
 
 -- insertion dans la table produit
 
-INSERT INTO `projet`.`produit` (`codeP`, `nomP`, `categorieP`,`quantite`,`stockAct`,`stockMin`,`stockMax`,`telF`) VALUES ('FP330', 'MiamLand', '0126339604');
+INSERT INTO `projet`.`produit` (`nomP`,`categorieP`,`quantite`,`stockAct`,`stockMin`,`stockMax`,`idF`) VALUES ('saumon', 'poisson', 12,10,3,20,'FU155');
+
+
+-- insertion dans la table client
+
+INSERT INTO `projet`.`client` (`idC`,`nomC`,`telC`) VALUES (15234,'Bernard',0651249903);
+
+
+-- insertion dans la table CdR
+
+INSERT INTO `projet`.`cdr` (`idCdR`,`cook`,`idC`) VALUES (14,300,15234);
+
+
+-- insertion dans la table recette
+
+INSERT INTO `projet`.`recette` (`idR`,`nomR`,`listeIngredients`,`descriptionR`,`prixR`,`remunerationCuisinier`,`idCdR`) VALUES (1,'tiramisu','oeufs,sucre,vanille,mascarpone,biscuits à la cuillère,café,cacao','dessert',4.99,5,14);
+
+
+-- insertion dans la table pointcook
+
+INSERT INTO `projet`.`pointcook` (`idPointCook`,`nbCook`,`idCdR`) VALUES (42,300,14);
+
+-- insertion dans la table cooking
+
+INSERT INTO `projet`.`cooking` (`idCooking`) VALUES (65);
